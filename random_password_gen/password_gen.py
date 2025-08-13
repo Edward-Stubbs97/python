f6311eefc2a3fc2ec79515f61ec987e7dbd9ad58
EdwardStubbs <edward.stubbs97@gmail.com>
2025-08-13 11:56:18 +0100

    comments

--- random_password_gen/password_gen.py
+++ random_password_gen/password_gen.py
@@ -19,22 +19,22 @@ def generate_password(min_length, numbers=True, special_characters=True):
     has_number = False
     has_special = False
 
-    while not meet_criteria or len(pwd) < min_length:
-        new_char = random.choice(characters)
-        pwd += new_char
+    while not meet_criteria or len(pwd) < min_length: # while either of these are true we continue so if we do not meet the criteria or length is too short
+        new_char = random.choice(characters) # randomly generate a new character 
+        pwd += new_char # add that to password
 
-        if new_char in digits:
+        if new_char in digits: # then we determin if the character added is a number or special character and set to true for either case
             has_number = True
         elif new_char in special:
             has_special = True
 
         meet_criteria = True
         if numbers:
-            meet_criteria = has_number
-        if special_characters:
-            meet_criteria = meet_criteria and has_special
+            meet_criteria = has_number # if we should include a number but we don't have one set meet criteria to false, if we need to include a number and we do it will stay true
+        if special_characters: 
+            meet_criteria = meet_criteria and has_special # " same as above for special character
 
-    return pwd 
+    return pwd # when all criteria is met wwe return the password pwd
 
 min_length = int(input("Enter the minimum length: "))
 has_number = input("Do you want to have numbers (y/n) ? ").lower() == "y"
