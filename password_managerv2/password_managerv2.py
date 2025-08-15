4886bfeff9c608a25f320bdbfba72ced40cbd0c2
EdwardStubbs <edward.stubbs97@gmail.com>
2025-08-15 11:58:59 +0100

    quit and re run

--- password_managerv2/password_managerv2.py
+++ password_managerv2/password_managerv2.py
@@ -1,26 +1,37 @@
-def password_manager(requirement):
+def password_manager():
     
     passwords = {
-        "instagram": "Rollo1997!",
-        "facebook": "Rollo7991!",
+        "instagram": "stubbsy901",
+        "facebook": "stubbsy901!",
     }
     
-    if requirement == "add":
+    while True:
         
-        site = input("What site is the password for? ").lower()
-        password = input("What is the password? ")
-        passwords[site] = password
-        print(f"Your passwords object has ben updated {passwords}")
+        requirement = input("Please type add or require: ").lower()
         
-    elif requirement == "require":
+        if requirement == "add":
+            
+            site = input("What site is the password for? ").lower()
+            password = input("What is the password? ")
+            passwords[site] = password
+            print(f"Your passwords object has ben updated {passwords}")
+            
+        elif requirement == "require":
+            
+            required = input("What site do you need? ").lower()
+            
+            if required in passwords:
+                print(f"Your password for {required} is {passwords[required]}")
         
-        required = input("What site do you need? ").lower()
-        print(f"Your password for {required} is {passwords[required]}")
-    
-    else:
+            else:
+                print("Site not found")
         
-        print("That is not a recognised command:")
+        elif requirement == "quit":
+            print("Exiting password manager.")
+            break
         
+        else:
+            print("That is not a recognised command.")
+
         
-requirement = input("Please type add or require ").lower()
-password_manager(requirement)
+password_manager()
